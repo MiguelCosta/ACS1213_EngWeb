d6209108980f443349ac08998331cc042db19605
Hugo Frade <emanspace@gmail.com>
2013-02-23 00:10:04 +0000

    Sobre a Empresa view fix

--- Code/Yii/site/protected/views/site/pages/about.php
+++ Code/Yii/site/protected/views/site/pages/about.php
@@ -1,12 +1,46 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name . ' - About';
+$this->pageTitle=Yii::app()->name . ' - Sobre a Empresa';
 $this->breadcrumbs=array(
-	'About',
+	'Sobre da Empresa',
 );
 ?>
-<h1>About</h1>
+<h1>Sobre a Empresa</h1>
+<h2>
+<b>Nome: </b>
+<span>Vidriluz - Fonsecas &amp; Frade, Lda.</span>
+</h2>
+<h2>Quem Somos?</h2>
+<p>Esta empresa dedica-se ao corte de vidro e construção de vidro duplo.<p>
 
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<p>Tendo em conta que emprega os melhores profissionais neste campo associa-se também a Aislaglas, uma marca de vidro duplo isolante.</p>
+
+<p>Assim sendo, realiza obras de médio e grande porte, que são previamente analisadas para que as sua elaboração seja a melhor, rápida e económica possível.</p>
+
+
+<br/>
+
+<h2>Ano de Fundação</h2>
+<p>1976</p>
+<br/>
+
+
+<h2>Certificações</h2>
+<ul>
+<li>Certificada com a marca Aislaglas (vidro duplo isolante)</li>
+</ul>
+<br/>
+
+<h2>Serviços Prestados</h2>
+<ul>
+<li>Colocação de vidro</li>
+<li>Biselagem (lapidação)</li>
+<li>Arestas</li>
+<li>Ricom</li>
+<li>Fabrico de vidro duplo</li>
+<li>Curvatura</li>
+<li>Colocação de vidros temperados</li>
+</ul>
+
+
