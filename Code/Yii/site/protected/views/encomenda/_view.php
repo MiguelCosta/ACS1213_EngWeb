18498070e830c3555b6acdac1bd30be472d71dce
Hugo Frade <emanspace@gmail.com>
2013-02-23 00:56:02 +0000

    encomendas index fix

--- Code/Yii/site/protected/views/encomenda/_view.php
+++ Code/Yii/site/protected/views/encomenda/_view.php
@@ -5,6 +5,8 @@
 
 <div class="view">
 
+	<h3>Encomenda <?php echo CHtml::link(CHtml::encode($data->serie).'/'.CHtml::encode($data->numero), array('view', 'id'=>$data->id));?> </h3>
+<!--
 	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
 	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
 	<br />
@@ -17,12 +19,12 @@
 	<?php echo CHtml::encode($data->numero); ?>
 	<br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('data')); ?>:</b>
-	<?php echo CHtml::encode($data->data); ?>
-	<br />
-
 	<b><?php echo CHtml::encode($data->getAttributeLabel('anexos')); ?>:</b>
 	<?php echo CHtml::encode($data->anexos); ?>
+	<br />
+-->
+	<b><?php echo CHtml::encode($data->getAttributeLabel('data')); ?>:</b>
+	<?php echo CHtml::encode($data->data); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('estado')); ?>:</b>
